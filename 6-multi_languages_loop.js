df01a6c8073376bd3b9281c63245813474795acf
cyuzuzo christian <christianhissan@gmail.com>
2025-11-07 15:19:07 +0200

    Move multi_languages_loop script into correct repo

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+const languages = [
+  'C is fun',
+  'Python is cool',
+  'JavaScript is amazing'
+];
+
+let output = '';
+
+for (const line of languages) {
+  output += line + '\n';
+}
+
+console.log(output.trim());
