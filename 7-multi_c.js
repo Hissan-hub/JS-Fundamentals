f39ab1aa30d303cbb04535137305c9bce4c827cd
cyuzuzo christian <christianhissan@gmail.com>
2025-11-07 17:17:38 +0200

    Add 7-multi_c.js: print C is fun x times with error handling

---
+++ 7-multi_c.js
@@ -0,0 +1,12 @@
+const [, , arg] = process.argv;
+const times = parseInt(arg, 10);
+
+if (!arg || isNaN(times) || times < 1) {
+  console.log('Missing number of occurrences');
+} else {
+  let i = 0;
+  while (i < times) {
+    console.log('C is fun');
+    i++;
+  }
+}
